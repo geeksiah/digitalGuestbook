c084f460da82bec2e71ccd94c97d0daeff421cd7
geeksiah <kojocreateve@gmail.com>
2026-02-08 06:52:53 +0000

    Template Assignment Logic4

--- backend/dist/utils/template-helper.js
+++ backend/dist/utils/template-helper.js
@@ -1,11 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getEventTemplate = getEventTemplate;
-const prisma_js_1 = require("../utils/prisma.js");
+const prisma_js_1 = __importDefault(require("../utils/prisma.js"));
 async function getEventTemplate(templateType, assignedTemplateId) {
     // 1. Try assigned template first
     if (assignedTemplateId) {
-        const assigned = await prisma_js_1.prisma.template.findUnique({
+        const assigned = await prisma_js_1.default.template.findUnique({
             where: { id: assignedTemplateId }
         });
         if (assigned && assigned.type === templateType) {
@@ -13,7 +16,7 @@ async function getEventTemplate(templateType, assignedTemplateId) {
         }
     }
     // 2. Fallback to default
-    return await prisma_js_1.prisma.template.findFirst({
+    return await prisma_js_1.default.template.findFirst({
         where: { type: templateType, isDefault: true }
     });
 }
